5c8572416907637a37540d732418db6fbc131343
Aaron Birkland <apb@jhu.edu>
2018-11-13 16:20:22 -0500

    Break out repositories migrator, and migrate all
    * Updates ERIC and DEC text
    * Updates repositoryKey on all repos

--- src/main/java/org/dataconservancy/pass/data/DataMigration.java
+++ src/main/java/org/dataconservancy/pass/data/DataMigration.java
@@ -37,6 +37,8 @@ import org.dataconservancy.pass.model.support.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.dataconservancy.pass.data.RepositoryMigrator.migrateRepositories;
+
 /*
  * Copyright 2018 Johns Hopkins University
  *
@@ -121,7 +123,7 @@ public class DataMigration {
             */
             
             migrateSubmissionModel();
-            migrateRepository();
+            migrateRepositories(client);
             migrateUsers();
             migrateGrants();
             migrateFunders();
@@ -312,72 +314,7 @@ public class DataMigration {
             unsuccessfulObjects++;
         }
     }
-
-    private static void migrateRepository() {
-    	String baseUrl = FedoraConfig.getBaseUrl();
-        URI jscholarshipRepoUri;
-        try {
-            jscholarshipRepoUri = new URI(baseUrl + "repositories/41/96/0a/92/41960a92-d3f8-4616-86a6-9e9cadc1a269");
-            Repository repository = client.readResource(jscholarshipRepoUri, Repository.class);
-            
-            String formSchema = "{"  
-                        + "\"id\":\"JScholarship\","
-                        + "\"schema\":{"
-                           + "\"title\":\"Johns Hopkins - JScholarship <br><p class='lead text-muted'>Deposit requirements for JH's institutional repository JScholarship.</p>\","
-                           + "\"type\":\"object\","
-                           + "\"properties\":{"
-                              + "\"authors\":{"
-                                 + "\"title\":\"<div class='row'><div class='col-6'>Author(s) <small class='text-muted'>(required)</small></div><div class='col-6 p-0'></div></div>\","
-                                 + "\"type\":\"array\","
-                                 + "\"uniqueItems\":true,"
-                                 + "\"items\":{"
-                                     + "\"type\":\"object\","
-                                     + "\"properties\":{"
-                                         + "\"author\":{"
-                                             + "\"type\":\"string\","
-                                             + "\"fieldClass\":\"body-text col-6 pull-left pl-0\""
-                                       + "}"
-                                    + "}"
-                                 + "}"
-                              + "}"
-                           + "}"
-                         + "},"
-                        + "\"options\":{"
-                           + "\"fields\":{"
-                              + "\"authors\":{"
-                                 + "\"hidden\":false"
-                              + "}"
-                           + "}"
-                        + "}"
-                     + "}";
-            
-            String agreementText = "NON-EXCLUSIVE LICENSE FOR USE OF MATERIALS This non-exclusive license defines the terms for the deposit of Materials in all formats "
-                    + "into the digital repository of materials collected, preserved and made available through the Johns Hopkins Digital Repository, JScholarship. "
-                    + "The Contributor hereby grants to Johns Hopkins a royalty free, non-exclusive worldwide license to use, re-use, display, distribute, transmit, "
-                    + "publish, re-publish or copy the Materials, either digitally or in print, or in any other medium, now or hereafter known, for the purpose of including "
-                    + "the Materials hereby licensed in the collection of materials in the Johns Hopkins Digital Repository for educational use worldwide. In some cases, access "
-                    + "to content may be restricted according to provisions established in negotiation with the copyright holder. This license shall not authorize the commercial "
-                    + "use of the Materials by Johns Hopkins or any other person or organization, but such Materials shall be restricted to non-profit educational use. Persons "
-                    + "may apply for commercial use by contacting the copyright holder. Copyright and any other intellectual property right in or to the Materials shall not be "
-                    + "transferred by this agreement and shall remain with the Contributor, or the Copyright holder if different from the Contributor. Other than this limited "
-                    + "license, the Contributor or Copyright holder retains all rights, title, copyright and other interest in the images licensed. If the submission contains "
-                    + "material for which the Contributor does not hold copyright, the Contributor represents that s/he has obtained the permission of the Copyright owner to "
-                    + "grant Johns Hopkins the rights required by this license, and that such third-party owned material is clearly identified and acknowledged within the text "
-                    + "or content of the submission. If the submission is based upon work that has been sponsored or supported by an agency or organization other than Johns Hopkins, "
-                    + "the Contributor represents that s/he has fulfilled any right of review or other obligations required by such contract or agreement. Johns Hopkins will not "
-                    + "make any alteration, other than as allowed by this license, to your submission. This agreement embodies the entire agreement of the parties. No modification "
-                    + "of this agreement shall be of any effect unless it is made in writing and signed by all of the parties to the agreement.";
-            
-            repository.setAgreementText(agreementText);
-            repository.setFormSchema(formSchema);
-            client.updateResource(repository);
-        } catch (Exception ex) {
-            LOG.error("Could not update JScholarship agreementText", ex.getMessage());
-            throw new RuntimeException(ex);
-        }
-        
-    }
-    
+ 
     private static Consumer<URI> delete = (id) -> {
         dump(deletedDir, id);
         client.deleteResource(id);
